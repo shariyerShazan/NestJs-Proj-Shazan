ec9027274fc55222b288347d3a5f9ef0a503bc6a
shariyerShazan <shariyershazan1@gmail.com>
2025-11-21 00:54:32 +0600

    student post patch get getByIdd and delete

--- nest-mongodb/src/student/student.controller.ts
+++ nest-mongodb/src/student/student.controller.ts
@@ -1,6 +1,6 @@
 import { Student } from './schema/student.schema';
 import { StudentService } from './student.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('student')
 export class StudentController {
@@ -9,5 +9,25 @@ export class StudentController {
   @Post()
   async createStudent(@Body() data: Partial<Student>) {
     return this.studentService.createStudent(data);
+  }
+
+  @Get()
+  async getAllStudents(){
+    return this.studentService.getAllStudents();
+  }
+
+  @Get('/:studentId')
+  async getStudentById(@Param('studentId') studentId : string){
+    return this.studentService.getStudentById(studentId);
+  }
+
+  @Patch('/:studentId')
+  async updateStudent(@Param('studentId') studentId : string , @Body() data : Partial<Student>){
+    return this.studentService.updateStudent(studentId , data);
+  }
+
+  @Delete('/:studentId')
+  async deleteStudent(@Param('studentId') studentId : string){
+    return this.studentService.deleteStudent(studentId);
   }
 }
--- nest-mongodb/src/student/student.service.ts
+++ nest-mongodb/src/student/student.service.ts
@@ -18,4 +18,44 @@ export class StudentService {
       throw new InternalServerErrorException(error);
     }
   }
+
+  async getAllStudents(): Promise<Student[]> {
+    try {
+      const students = this.studentModel.find().exec();
+      return students;
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async getStudentById(studentId: string ) : Promise<Student | null> {
+    try {
+      const student = this.studentModel.findById(studentId).exec()
+      return student;
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async updateStudent(studentId : string , data : Partial<Student>) : Promise<Student | null> {
+    try {
+      const student = this.studentModel.findByIdAndUpdate(studentId , data , {new : true}).exec()
+      return student;
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteStudent(studentId : string): Promise<Student | null> {
+    try {
+      const deletedStudent = this.studentModel.findByIdAndDelete(studentId).exec()
+      return deletedStudent;
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException(error);
+    }
+  }
 }
